6b10893a8095c365d978fedcdec9df77a2f88313
fmpreferences <57734230+fmpreferences@users.noreply.github.com>
2025-05-01 18:27:48 -0500

    Site maintenance message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
 <form action="/game">
     <input type="submit" value="Start" />
+    <h1>IMPORTANT: DB MAINTENANCE GOING ON. THINGS MAY NOT WORK!</h1>
+
+    <h1>FAQ:</h1>
+    <h3>WTF is this?</h3>
+    <p>My version of <a href="https://www.higherorlowergame.com/spotify/songs/">Higher or Lower: Spotify</a>. The issue with the original was the genre categorization was suspect, there were random songs with 69 million listens because of the way things were categorized. In addition, it was only updated yearly. Try the genre organization yourself!</p>
+    
+    <h3>Where are you getting this data?</h3>
+    <p>Spotify is protective of its data, this is mirrored from <a href="https://kworb.net/spotify/toplists.html">kworb.net</a>, which has many limitations.</p>
+
+    <h3>Song X is miscategorized, can you fix it?</h3>
+    <p>Understand I dont want to do too much manual work here due to the algorithm that automatically sorts and updates new songs. However, if you genuinely think it's too egregious, open an issue on the <a href="https://github.com/fmpreferences/spotgame-data">database github</a>.</p>
+
+    <h3>Song X is missing in the database!</h3>
+    <p>Unfixable by me, contact kworb, although expect to be ghosted.</p>
+
+    <h3>Song X is not the right number of listens!</h3>
+    <p>This is probably because my "reconstruction" algorithm is too conservative, open an issue on the <a href="https://github.com/fmpreferences/spotgame-data">database github</a>.</p>
+
+    <h3>Can you consider adding genre X?</h3>
+    <p>I'm not a music expert, but if you really think there is enough songs to make a whole other category, open an issue on the <a href="https://github.com/fmpreferences/spotgame-data">database github</a>.</p>
 </form>
