ff512a644d1c300f37b964ee561d9eaaca091ecf
fmpreferences <57734230+fmpreferences@users.noreply.github.com>
2025-05-12 12:31:19 -0500

    see if distribution of stuff changes

--- src/routes/api/1.0/get-random-songs/+server.ts
+++ src/routes/api/1.0/get-random-songs/+server.ts
@@ -13,7 +13,7 @@ export const POST: RequestHandler = async ({ request, platform }) => {
 
     const rows = await platform?.env.DB.prepare(
         `select distinct a.title, a.id, artist_name, album_art, artist_index from tracks a join track_artists b on a.id = b.id join artists c on b.artist_id = c.artist_id
-        where a.id in (select d.id from tracks d join buckets e on d.id = e.id where date between ? and ? and 
+        where a.id in (select distinct d.id from tracks d join buckets e on d.id = e.id where date between ? and ? and 
         streams >= ? and bucket in (${active.map((e: any) => `'${e.genre}'`)}) order by random() limit ?)`
     ).bind(`${min_year}-01-01`, `${max_year}-12-31`, min_streams, n).run();
 
